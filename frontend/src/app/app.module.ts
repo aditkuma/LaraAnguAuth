99795ce749c7b2afbdd894db0afc6beb3963d3ad
Aditya <aditkuma@gmail.com>
2019-05-11 11:51:10 +0530

    Configuration of Routes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { RequestResetComponent } from './components/password/request-reset/reque
     LoginComponent,
     SignupComponent,
     ProfileComponent,
-    RequestResetComponent
+    RequestResetComponent,
+    ResponseResetComponent
   ],
   imports: [
     BrowserModule,
