33f88933f4346c30170e9a77078c923d7e468ed6
Aditya <aditkuma@gmail.com>
2019-05-17 23:59:22 +0530

    Signup Functionality Process

--- backend/app/User.php
+++ backend/app/User.php
@@ -51,4 +51,8 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+    public function setPasswordAttribute($value)
+    {
+        return $this->attributes['password'] = bcrypt($value); 
+    }
 }
