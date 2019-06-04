296384142e42ee89ff4b57869ef6346bc5311265
aditkuma <aditkuma@gmail.com>
2019-06-04 19:40:04 +0530

    Handling JWT token in Frontend

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { JarwisService } from 'src/app/Services/jarwis.service';
+import { TokenService } from 'src/app/Services/token.service';
 
 @Component({
   selector: 'app-login',
@@ -16,14 +17,17 @@ export class LoginComponent implements OnInit {
   public error=null;
   
   
-  constructor(private Jarwis:JarwisService) { }
+  constructor(private Jarwis:JarwisService, private Token:TokenService) { }
   onSubmit()
   {
     //console.log(this.form);
     this.Jarwis.login(this.form).subscribe(
-      data => console.log(data),
+      data => this.handleResponse(data),
       error => this.handleError(error)
     );
+  }
+  handleResponse(data){
+    this.Token.handle(data.access_token);
   }
   handleError(error){
     this.error = error.error.error;
